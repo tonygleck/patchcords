18b5c8f08e3c58af6b041249ad2e7aadca10fe34
Jelani Brandon <jebrando@microsoft.com>
2021-04-04 16:19:32 -0700

    chore: removing vld.h header

--- tests/cord_client_winsock_ut/main.c
+++ tests/cord_client_winsock_ut/main.c
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <vld.h>
-
 #include "ctest.h"
 
 int main(void)
